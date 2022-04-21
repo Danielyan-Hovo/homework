8b5b0de0559ebf18409ab82800ae7fe3b706f629
Danielyan-Hovo <danielyan.hovo.2000@gmail.com>
2022-04-21 18:25:39 +0400

    my Promise class implementation

---
+++ myPromise.js
@@ -0,0 +1,63 @@
+function noop(){};
+
+class MyPromise{
+    constructor(executor) {
+        this.queue = [];
+        this.errorHandler = noop;
+        this.finallyHandler = noop;
+
+        try {
+            executor.call(null,this.onResolve.bind(this),this.onReject.bind(this))
+        }catch (e) {
+            this.errorHandler(e);
+        }finally {
+            this.finallyHandler();
+        }
+    }
+
+    onResolve(data){
+        this.queue.forEach(callback=>{
+            data = callback(data);
+        })
+        this.finallyHandler()
+    }
+
+    onReject(error){
+        this.errorHandler(error);
+
+        this.finallyHandler()
+    }
+
+    then(fn){
+        this.queue.push(fn);
+        return this;
+    }
+
+    catch(fn){
+        this.errorHandler = fn;
+        return this;
+    }
+
+    finally(fn){
+        this.finallyHandler = fn;
+        return this;
+    }
+}
+
+
+const promise = new MyPromise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve('MY PROMISE')
+    },500)
+})
+
+promise
+    .then(course=>{
+        console.log('Danielyan Hovo', course)
+    })
+    .catch(err=>{
+        console.log('Error',err)
+    })
+    .finally(()=>{
+        console.log('My Promise Finally')
+    })
